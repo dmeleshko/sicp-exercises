13cbd47a8b73ac1d398786f9ee5bc30ec7bd1c44
Dmitriy Meleshko <dmitriy.meleshko@gmail.com>
2018-03-19 17:59:17 +0600

    exercise 1.3

---
+++ chapter1/ex1_03.scm
@@ -0,0 +1,12 @@
+(define (square x) (* x x))
+
+(define (sum-of-squares x y) (+ (square x) (square y)))
+
+(define (max x y)
+      (if (> x y) x y))
+
+(define (min x y)
+      (if (< x y) x y))
+
+(define (max-sum-of-squares x y z)
+      (sum-of-squares (max x y) (max (min x y) z)))
